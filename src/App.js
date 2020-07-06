7a19fde68f420d8a2a0bed9fccf11abfbc6e027f
unknown <aryan8931080555@gmail.com>
2020-07-06 19:35:54 +0530

    router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import Home from './screens/Home'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Login from './screens/Login'
+import Signup from './screens/Signup'
 function App() {
   return (
     <Router>
     <Route path="/" component={Home} />
+    <Route path="/login" component={Login} />
+    <Route path="/signup" component={Signup} />
   </Router>
   );
 }
